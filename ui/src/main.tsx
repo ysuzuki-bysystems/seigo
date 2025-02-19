85f5840c1160f0b2735cd5d1b186c25cddb459c2
ysuzuki-bysystems <ysuzuki@bysystems.jp>
2025-02-19 18:03:06 +0900

    Shrink ui

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+function ban(): never {
+  throw new Error();
+}
+
+createRoot(document.getElementById('root') ?? ban()).render(
   <StrictMode>
     <App />
   </StrictMode>,
