5a0ab3ee4f0f2d0af9594eede070301465b23453
ysuzuki-bysystems <ysuzuki@bysystems.jp>
2025-02-26 17:30:32 +0900

    uninstall eslint

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 
 function ban(): never {
-  throw new Error();
+  throw new Error("ban");
 }
 
 createRoot(document.getElementById("root") ?? ban()).render(
