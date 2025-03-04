41083fab8041f9dfb1b377c87b5b2dba74b682a9
ysuzuki-bysystems <ysuzuki@bysystems.jp>
2025-03-04 15:15:53 +0900

    mv web{,_dev}.go internal/web/

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/ysuzuki-bysystems/seigo/internal/web"
 )
 
 func main() {
@@ -16,7 +17,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("*", static())
+	e.GET("*", web.Static())
 
 	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		slog.Error("Failed to serve.", "err", err)
