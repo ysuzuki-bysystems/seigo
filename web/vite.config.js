8f43056079cd7608a97b334def0bf35ea9eea0ea
ysuzuki-bysystems <ysuzuki@bysystems.jp>
2025-03-12 13:24:51 +0900

    Fix lint

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
